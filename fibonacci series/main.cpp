b552b187a11e88cd15fbb43ae3a9bb151fbeaabf
giacomo98 <giacomobaruzzi98@gmail.com>
2014-04-16 17:16:55 +0200

    Initial Commit

---
+++ fibonacci series/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  fibonacci series
+//
+//  Created by giacomo baruzzi on 16/04/14.
+//  Copyright (c) 2014 giacomo baruzzi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
